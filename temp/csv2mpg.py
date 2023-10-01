0da6e3bd106869e071828b3c1f796b01ff3816e5
Ishu Singh <ishu.111636@yahoo.com>
2023-10-01 23:37:11 +0530

    connect to onedrive

--- temp/csv2mpg.py
+++
@@ -1,9 +0,0 @@
-import csv
-import os
-import re
-
-with open('instastory.csv', 'r') as f:
-    data = csv.DictReader(f)
-
-    for line in data:
-        print(line['ï»¿Topic'], line['Part 1'].encode("utf-8", "ignore").decode(), line['Part 2'].encode('utf-8', 'ignore'))
